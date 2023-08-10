78e937da04e8825dbb10b22c0bd0c6dd70c47b94
minhaj123456 <133559085+minhaj123456@users.noreply.github.com>
2023-08-10 13:44:00 +0530

    op

---
+++ bin/leapyearornot.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+void main(List<String> arguments) {
+  print('enter the year');
+  var year=int.parse(stdin.readLineSync()!);
+  if(year%4==0)
+  {
+    if(year%100==0)
+    {
+      if(year%400==0)
+      {
+        print("$year,is a leap year");
+      }
+    else
+{
+  print("$year,is not a leap year");
+}
+    }
+    else 
+    {
+       print("$year,is a leap year");
+    } 
+  }
+    else
+{
+  print("$year,is not a leap year");
+  }
+}
+  
